0f372ec3603d7fa89004795e35ca1cccb9028ec9
revanrn <59168101+revanrn@users.noreply.github.com>
2020-01-17 16:20:40 +0530

    Update TrivagoLoginTesting.java

--- TrivagoLoginTesting.java
+++ TrivagoLoginTesting.java
@@ -1,35 +1,31 @@
-package DeloitteTrivago;
+package SeleniumDeloitteDemoPackage;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TrivagoLoginTesting {
-	public static void main(String args[]) {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\Downloads\\chromedriver.exe");
+public class Trivago {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Admin\\Downloads\\chromedriver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		driver.get("https://Trivago.com");
 		driver.manage().window().maximize();
-		driver.get("https://www.trivago.in/");
-		driver.findElement(By.cssSelector("#js_navigation > div > div.pos-relative.has-popover > button")).click();
-		driver.findElement(By.id("check_email")).sendKeys("revanrn96@gmail.com");
-		driver.findElement(By.id("login_email_submit")).click();
-		driver.findElement(By.id("login_password")).sendKeys("Trivago2020");
+		driver.findElement(By.cssSelector("#js_navigation > div > div.pos-relative.has-popover > button > span")).click();
+		driver.findElement(By.xpath("//*[@id=\"check_email\"]")).sendKeys("revanrn96.com");
+		driver.findElement(By.cssSelector("#login_email_submit")).click();
 		try {
 			Thread.sleep(4000);
-		}
-		catch(InterruptedException e){
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		driver.findElement(By.id("login_password")).sendKeys("makemytrip2020");
-		try {
-			Thread.sleep(400);
-		}
-		catch(InterruptedException e){
-			e.printStackTrace();
-		}
-		driver.findElement(By.id("login_submit")).click();		
+		driver.findElement(By.xpath("//*[@id=\"login_password\"]")).sendKeys("Trivago2020");
+		driver.findElement(By.cssSelector("#login_submit")).click();
+		
+		
 		
 	}
+
 }
-
-
-
