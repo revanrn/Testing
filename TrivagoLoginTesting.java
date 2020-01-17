72371a29d5a5a28be663dec1c6dfa099c4f51e6a
revanrn <59168101+revanrn@users.noreply.github.com>
2020-01-17 16:24:23 +0530

    Update TrivagoLoginTesting.java

--- TrivagoLoginTesting.java
+++ TrivagoLoginTesting.java
@@ -1,19 +1,19 @@
-package SeleniumDeloitteDemoPackage;
+package DeloitteTrivago;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Trivago {
+public class TrivagoLoginTesting {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\Admin\\Downloads\\chromedriver\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Admin\\Downloads\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://Trivago.com");
 		driver.manage().window().maximize();
 		driver.findElement(By.cssSelector("#js_navigation > div > div.pos-relative.has-popover > button > span")).click();
-		driver.findElement(By.xpath("//*[@id=\"check_email\"]")).sendKeys("revanrn96.com");
+		driver.findElement(By.xpath("//*[@id=\"check_email\"]")).sendKeys("revanrn96@gmail.com");
 		driver.findElement(By.cssSelector("#login_email_submit")).click();
 		try {
 			Thread.sleep(4000);
